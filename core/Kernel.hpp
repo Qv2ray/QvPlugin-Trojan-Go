ad0d9723484a1cb2164b0fc27d119875b1477367
QxQ <59914293+U-v-U@users.noreply.github.com>
2020-10-15 13:52:10 +0800

    BREAKING: added GetKernelName

--- core/Kernel.hpp
+++ core/Kernel.hpp
@@ -12,6 +12,10 @@ class QvTrojanGoPluginKernel : public Qv2rayPlugin::PluginKernel
     void SetConnectionSettings(const QMap<Qv2rayPlugin::KernelOptionFlags, QVariant> &settings, const QJsonObject &connectionInfo) override;
     bool StartKernel() override;
     bool StopKernel() override;
+    QString GetKernelName() const override
+    {
+        return "TrojanGo";
+    }
 
   private slots:
     void OnProcessOutputReadyRead(int);
